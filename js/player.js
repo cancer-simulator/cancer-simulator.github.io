fff30bd1dc54b78ed042e8f8604b847d9f8568b9
Alexei Fedotov <alexei.fedotov@gmail.com>
2018-10-26 18:00:41 +0300

    added medulloblastoma

--- js/player.js
+++ js/player.js
@@ -1,5 +1,7 @@
 Player = {}
 
+Player.AGE_STEP = 1.0 / 365
+
 Player.init = function() {
 	this.male = (Math.random() < 0.6) ? 0 : 1
 	UI.notifyPositive('A nice ' +
@@ -9,15 +11,21 @@ Player.init = function() {
 	this.hormone = 30
 	this.adolescenceStage = 0
 	this.savings = 0
-	this.granny = 10
-	this.extortion = 1
+    this.weight = 3 + Math.random() * 2
+    this.props = {}
 }
 
 Player.update = function() {
-	this.age += 1.0 / 365
-	this.savings *= 1.001
-	console.log(this.age)
+	this.age += this.AGE_STEP
+    if (this.age < 20) {
+        this.weight += this.AGE_STEP * Math.random() * (6 + this.male)
+    } else {
+        this.weight += this.AGE_STEP * Math.random()
+    }
+	this.savings *= 1.0001
+
 	UI.setVal('age', this.age.toFixed(2))
 	UI.setVal('sex', (this.male) ? '♂' : '♀')
+    UI.setVal('weight', this.weight.toFixed(0))
 	UI.setVal('savings', this.savings.toFixed(2))
 }
